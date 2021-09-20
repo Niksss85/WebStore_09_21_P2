a7f3d84bed4a5403eabbdd39e16d4123cbc97553
Nikolay <kol-pav@mail.ru>
2021-09-20 14:46:44 +0300

    Заготовка контроллера управления сотрудниками

---
+++ Services/WebStore.WebAPI/Controllers/EmployeesApiController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/employees")]//api/EmployeesApi
+
+    public class EmployeesApiController : ControllerBase
+    {
+        private readonly IEmployeesData _EmployeesData;
+        public EmployeesApiController(IEmployeesData EmployeesData) =>_EmployeesData = EmployeesData;
+
+
+
+    }
+}
