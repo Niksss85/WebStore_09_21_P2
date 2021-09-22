66f0198524ae9c3ae469f85750a1229d99a4f6e8
Nikolay <kol-pav@mail.ru>
2021-09-22 16:39:10 +0300

    Загатовка контроллеров WebApi для системы identity

--- Services/WebStore.WebAPI/Controllers/Identity/RolesApiController.cs
+++ Services/WebStore.WebAPI/Controllers/Identity/RolesApiController.cs
@@ -1,16 +1,29 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
 using WebStore.Interfaces;
 
 namespace WebStore.WebAPI.Controllers.Identity
 {
-    [Route(WebAPIAddresses.Identity.Roles)]
     [ApiController]
+    [Route(WebAPIAddresses.Identity.Roles)]
     public class RolesApiController : ControllerBase
     {
+        private readonly RoleStore<Role> _RoleStore;
+
+        public RolesApiController(WebStoreDB db)
+        {
+            _RoleStore = new RoleStore<Role>(db);
+        }
+
+        [HttpGet("all")]
+        public async Task<IEnumerable<Role>> GetAllUsers() => await _RoleStore.Roles.ToArrayAsync();
     }
 }
--- Services/WebStore.WebAPI/Controllers/Identity/UsersApiController.cs
+++ Services/WebStore.WebAPI/Controllers/Identity/UsersApiController.cs
@@ -1,16 +1,32 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+
+using WebStore.Domain.Entities.Identity;
 using WebStore.Interfaces;
 
 namespace WebStore.WebAPI.Controllers.Identity
 {
-    [Route(WebAPIAddresses.Identity.Users)]
     [ApiController]
+    [Route(WebAPIAddresses.Identity.Users)]
     public class UsersApiController : ControllerBase
     {
+        public readonly UserStore<User, Role, WebStoreDB> _UserStore;
+
+        public UsersApiController(WebStoreDB db)
+        {
+            _UserStore = new UserStore<User, Role, WebStoreDB>(db);
+            //_UserStore.AutoSaveChanges = false; // выключение отслеживания изменений для повышения быстродействия
+        }
+
+        [HttpGet("all")]
+        public async Task<IEnumerable<User>> GetAllUsers() => await _UserStore.Users.ToArrayAsync();
+
     }
 }
