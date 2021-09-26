bee394eb5d55353ce32b1958176ce6b551a5fb64
Nik Pav <kol-pav@mail.ru>
2021-09-26 14:35:50 +0300

    Добавление документации для WEBAPI

--- Services/WebStore.WebAPI/Controllers/EmployeesApiController.cs
+++ Services/WebStore.WebAPI/Controllers/EmployeesApiController.cs
@@ -10,6 +10,7 @@ using WebStore.Interfaces.Services;
 
 namespace WebStore.WebAPI.Controllers
 {
+    /// <summary>API для управления сотрудниками</summary>
     [ApiController]
     [Route(WebAPIAddresses.Employees)]//api/EmployeesApi
 
@@ -18,13 +19,22 @@ namespace WebStore.WebAPI.Controllers
         private readonly IEmployeesData _EmployeesData;
         public EmployeesApiController(IEmployeesData EmployeesData) => _EmployeesData = EmployeesData;
 
+        /// <summary>Получение всех сотрудников</summary>
+        /// <returns>Список сотрудников</returns>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Employee>))]
         public IActionResult Get()
         {
             var employees = _EmployeesData.GetAll();
             return Ok(employees);
         }
+
+        /// <summary>Получение сотрудника по его идентификатору</summary>
+        /// <param name="id">Идентификатор сотрудника</param>
+        /// <returns>Информация о сотруднике с указанным идентификатором</returns>
         [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Employee))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(int))]
         public IActionResult GetById(int id)
         {
             var employee = _EmployeesData.Get(id);
@@ -32,19 +42,32 @@ namespace WebStore.WebAPI.Controllers
                 return NotFound(new { id });
             return Ok(employee);
         }
+
+        /// <summary>Добавление нового сотрудника</summary>
+        /// <param name="employee">Информация о добавляемом сотруднике</param>
+        /// <returns>Идентификатор добавленного сотрудника</returns>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int))]
         public IActionResult Add(Employee employee)
         {
             var id = _EmployeesData.Add(employee);
             return Ok(id);//CreatedAtAction(nameof(GetById) , new { id });
         }
+        /// <summary>Редактирование сотрудника</summary>
+        /// <param name="employee">Информация, которую надо внести в запись указанного сотрудника</param>
         [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult Update(Employee employee)
         {
             _EmployeesData.Update(employee);
             return Ok();
         }
+        /// <summary>Удаление сотрудника</summary>
+        /// <param name="id">Идентификатор удаляемого сотрудника</param>
+        /// <returns>Истина, если сотрудник успешно удалён</returns>
         [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(bool))]
         public IActionResult Delete(int id)
         {
             var result = _EmployeesData.Delete(id);
--- Services/WebStore.WebAPI/Controllers/OrdersApiController.cs
+++ Services/WebStore.WebAPI/Controllers/OrdersApiController.cs
@@ -8,8 +8,8 @@ using WebStore.Domain.DTO;
 using WebStore.Interfaces;
 using WebStore.Interfaces.Services;
 
-namespace WebStore.WebAPI.Controllers { 
-
+namespace WebStore.WebAPI.Controllers {
+    /// <summary>API системы заказов</summary>
     [ApiController]
     [Route(WebAPIAddresses.Orders)]
     public class OrdersApiController : ControllerBase
@@ -19,20 +19,34 @@ namespace WebStore.WebAPI.Controllers {
         {
             _OrderService = OrderService;
         }
+        /// <summary>Получить список заказов указанного пользователя</summary>
+        /// <param name="UserName">Имя пользователя</param>
+        /// <returns>Список заказов указанного пользователя</returns>
         [HttpGet("user/{UserName}")] //api/orders/user/Petrov
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<OrderDTO>))]
         public async Task<IActionResult> GetUserOrders(string UserName)
         {
             var orders = await _OrderService.GetUserOrder(UserName);
             return Ok(orders.ToDTO());
         }
-        [HttpGet("{id:int}")] 
+
+        /// <summary>Получить заказ по его идентификатору</summary>
+        /// <param name="id">Идентификатор заказа</param>
+        /// <returns>Информация по заказу с указанным идентификатором</returns>
+        
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(OrderDTO))]
         public async  Task<IActionResult> GetOrderById(int id)
         {
             var order = await _OrderService.GetOrderById(id);
             return Ok(order.ToDTO());
         }
+        /// <summary>Создание нового заказа</summary>
+        /// <param name="UserName">Имя пользователя, для которого оформляется заказ</param>
+        /// <param name="OrderModel">Информация о заказе</param>
+        /// <returns>Сформированный заказ</returns>
         [HttpPost("{UserName}")]
-
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(OrderDTO))]
         public async Task<IActionResult> CreateOrders(string UserName, [FromBody] CreateOrderDTO OrderModel)
         {
             var order = await _OrderService.CreateOrder(UserName, OrderModel.Items.ToCartView(), OrderModel.Order);
