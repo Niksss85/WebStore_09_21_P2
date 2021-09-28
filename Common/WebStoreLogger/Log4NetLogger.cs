59f37264d19cea3928c2b5a122a6710d2923394d
Nikolay <kol-pav@mail.ru>
2021-09-28 16:33:15 +0300

    Заготвка библиотеки системы логгирования

---
+++ Common/WebStoreLogger/Log4NetLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStoreLogger
+{
+    public class Log4NetLoggerFactory
+    {
+    }
+    public class Log4NetLoggerProvider
+    {
+    }
+    public class Log4NetLogger
+    {
+    }
+
+}
