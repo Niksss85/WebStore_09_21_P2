5b859e4b9e37746ecca3daf5bb0704d9a49d078f
Nikolay <kol-pav@mail.ru>
2021-09-30 14:46:57 +0300

    Добавление новых сервисов и интерфейсов корзины, а также реализация тестирования контроллера сервиса корзины

--- Services/WebStoreServices/Services/CartService.cs
+++ Services/WebStoreServices/Services/CartService.cs
@@ -8,7 +8,7 @@ using WebStore.Infrastructure.Mapping;
 using WebStore.Interfaces.Services;
 
 
-namespace WebStore.Services.InCookies
+namespace WebStore.Services.Services.InCookies
 {
     public class InCookiesCartService : ICartService
     {
