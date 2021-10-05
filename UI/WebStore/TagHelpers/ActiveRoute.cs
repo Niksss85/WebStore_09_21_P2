2c1a1e234c2bd0fe9e896741d3b1261981b62404
Nikolay <kol-pav@mail.ru>
2021-10-05 14:54:20 +0300

    логика работы таг-хелпера

--- UI/WebStore/TagHelpers/ActiveRoute.cs
+++ UI/WebStore/TagHelpers/ActiveRoute.cs
@@ -25,7 +25,45 @@ namespace WebStore.TagHelpers
         public ViewContext ViewContext { get; set; }
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+            if (IsActive())
+                MakeActive(output);
+
             output.Attributes.RemoveAll(AttributeName);
+        }
+        private bool IsActive()
+        {
+            var route_values = ViewContext.RouteData.Values;//адрес(маршрут)
+
+            var route_controller = route_values["controller"]?.ToString();
+            var route_action = route_values["action"]?.ToString();
+
+            //if (!string.IsNullOrEmpty(Action) && !string.Equals(Action, current_action))
+            //    return false;
+            if (Action is { Length: > 0 } action && !string.Equals(action, route_action))
+                return false;
+
+            if (Controller is { Length: > 0 } controller && !string.Equals(controller, route_controller))
+                return false;
+
+            foreach (var (key, value) in RouteValue)
+                if (!route_values.ContainsKey(key) || route_values[key]?.ToString() != value)
+                    return false;
+
+            return true;
+        }
+        private static void MakeActive(TagHelperOutput output)
+        {
+            var class_attribute = output.Attributes.FirstOrDefault(attr => attr.Name == "class");
+
+            if (class_attribute is null)
+                output.Attributes.Add("class", "active");
+            else
+            {
+                if (class_attribute.Value.ToString()?.Contains("active") ?? false)
+                    return;
+
+                output.Attributes.SetAttribute("class", class_attribute.Value + " active");
+            }
         }
     }
 }
